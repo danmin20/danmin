6e5851bab8c103d4cc8f5008540645acc30c7935
lee jeongmin <danmin20@khu.ac.kr>
2021-11-21 20:31:57 +0900

    [chore] styles

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+import "./reset.scss";
+import { theme } from "./theme";
+
+export const GlobalStyle = createGlobalStyle`
+    :focus {
+        outline: none;
+        border: none;
+    }
+    ::-webkit-scrollbar {
+        display: none;
+    }
+    html {
+        font-size: 10px;
+        -webkit-text-size-adjust: none;
+        font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+        font-display: fallback;
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+        color: ${theme.color.black};
+        background-color: #FFFFFF;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        body {
+            margin: 0;
+        }
+    }
+`;
